7c18361d8dc553024cd738af968434fc7b8b6d70
Timothy Lin <lzh9102@gmail.com>
2013-11-11 10:45:55 +0800

    initial commit

---
+++ thunderbird-compose.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl
+
+# configuration
+
+my $tabwidth = 3;
+
+# ---- end of configuration ----
+
+use strict;
+use warnings;
+use File::Temp;
+use Text::Markdown 'markdown';
+
+sub write_default_fields() {
+	open TF, ">", $_[0];
+	print TF <<EOF
+TO:
+CC:
+SUBJECT:
+ATTACHMENT:
+----- body below -----
+EOF
+	;
+	close TF;
+}
+
+sub encode_body() {
+	my $text = shift;
+	my $html = markdown($text, {tab_width => $tabwidth});
+	$html =~ s/\\\n//g; # concatenate lines separated with \<newline>
+	$html =~ s/\n/ /g; # join lines
+	return $html;
+}
+
+my $tmpfile;
+if (!@ARGV) {
+	$tmpfile = File::Temp->new();
+	&write_default_fields($tmpfile);
+} else {
+	$tmpfile = $ARGV[0];
+	&write_default_fields($tmpfile) unless (-e $tmpfile);
+}
+
+# call vim to edit file
+system('vim', $tmpfile);
+
+# read tmpfile back and process it
+open TF, "<", $tmpfile;
+my %args = ();
+
+while (<TF>) { # read header
+	my $line = $_;
+	if ($line =~ /^([a-zA-Z]+):\s*(.*)$/) {
+		my $key = lc($1);
+		my $value = $2;
+		$value =~ s/\s+$//; # remove trailing spaces
+		$value =~ s/\"/\\"/g;
+		$args{$key} = $value;
+	} else {
+		last;
+	}
+}
+my $body = "";
+while (<TF>) { # read body
+	$body .= $_;
+}
+close TF;
+
+# check recepients
+if ($args{"to"} =~ /^\s*$/) {
+	print "recepients not specified, exiting\n";
+	exit 1;
+}
+
+my $arg = "";
+for my $key (keys %args) {
+	$arg .= "$key='" . $args{$key} . "',";
+}
+$arg .= "body=" . &encode_body($body);
+
+# invoke thunderbird
+system('thunderbird', '--compose', $arg);
