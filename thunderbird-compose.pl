0b9e5174350ff54eacd7b01f4e51c38c5683250a
Timothy Lin <lzh9102@gmail.com>
2013-11-12 22:41:57 +0800

    Print hint to notice the user that body is markdown syntax

--- thunderbird-compose.pl
+++ thunderbird-compose.pl
@@ -29,7 +29,7 @@ sub write_default_fields() {
 	print TF "CC: " . $args{cc} . "\n";
 	print TF "SUBJECT: " . $args{subject} . "\n";
 	print TF "ATTACHMENT: " . $args{attachment} . "\n";
-	print TF "----- e-mail body -----\n";
+	print TF "----- body (markdown syntax) -----\n";
 	close TF;
 }
 
